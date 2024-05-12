04eeb4ea5fe0f29f695945cdb70a96b43353a65a
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-05-12 21:33:08 +0200

    Add darkmode to the 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,9 +6,9 @@ import CenterBox from './components/CenterBox'
 export default function NotFound() {
   return (
     <main>
-      <div className="w-full bg-white">
-        <div className="to z-50 grid h-dvh place-items-center content-center bg-gradient-to-b from-primary-100/90 to-transparent">
-          <h1 className="text-shadow bg-transparent text-[15vw] font-extrabold shadow-primary-500/70 md:shadow-primary-400/80">
+      <div className="w-full bg-white dark:bg-zinc-950">
+        <div className="to z-50 grid h-dvh place-items-center content-center bg-gradient-to-b from-primary-100/90 to-transparent dark:from-primary-900/50">
+          <h1 className="text-shadow bg-transparent text-[15vw] font-extrabold shadow-primary-500/70 md:shadow-primary-400/80 dark:text-zinc-300">
             404
           </h1>
           <CenterBox>
