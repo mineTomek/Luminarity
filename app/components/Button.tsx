91023c3ef7a83943ca5ba9f86f6c5267727c32f8
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-05-13 23:48:08 +0200

    Allow autowidth on buttons

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,10 +3,14 @@ import { MouseEventHandler } from 'react'
 export default function Button(props: {
   onClick: MouseEventHandler<HTMLButtonElement>
   children: JSX.Element | string
+  autoWidth?: boolean
 }) {
   return (
     <button
-      className="mx-auto w-[8rem] rounded-full bg-primary-100 p-3 shadow-inner dark:bg-zinc-800 dark:shadow-zinc-700"
+      className={
+        'mx-auto rounded-full bg-primary-100 p-3 shadow-inner dark:bg-zinc-800 dark:shadow-zinc-700 ' +
+        (!props.autoWidth && 'w-[8rem]')
+      }
       onClick={props.onClick}
     >
       {props.children}
